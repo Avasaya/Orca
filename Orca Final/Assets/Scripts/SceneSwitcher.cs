e47cea9aa5f532d4884950f5ce9bada79b6eebc7
Avasaya <adrian.vasaya@usjr.edu.ph>
2022-07-22 17:59:45 +0800

    Putting orca into git hub

---
+++ Orca Final/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitcher : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void backGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+   public void quit()
+    {
+        Application.Quit();
+    }
+}
